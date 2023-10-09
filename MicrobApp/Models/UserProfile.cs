e3192d289185a8ba772047551b21664d9e4d548e
Lucia <luligal@gmail.com>
2023-10-09 18:11:15 -0300

    Mueve logica al profilePage.xaml.cs, Data binding

---
+++ MicrobApp/Models/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrobApp.Models
+{
+    public class UserProfile
+    {
+        public string ProfileImage { get; set; } 
+        public string CoverImage { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Biography { get; set; }
+        public DateOnly JoinDate { get; set; }
+        public int Followers { get; set; }
+        public int Following { get; set; }
+        public bool IsFollowing { get; set; }
+    }
+}
--- MicrobApp/Views/ProfilePage.xaml.cs
+++ MicrobApp/Views/ProfilePage.xaml.cs
@@ -1,9 +1,31 @@
+using MicrobApp.Models;
+
 namespace MicrobApp.Views;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
-	{
-		InitializeComponent();
-	}
+    public UserProfile UserProfile { get; set; }
+
+    public ProfilePage()
+    {
+        InitializeComponent();
+        GetProfileData();
+    }
+
+    private void GetProfileData()
+    {
+        UserProfile = new UserProfile
+        {
+            ProfileImage = "images/modelprofile.png",
+            CoverImage = "timeline.png",
+            Name = "Nombre Apellido",
+            Username = "@persona@marciano",
+            Biography = "Digital Goodies Team - Web u Mobile UI/UX development; Graphics; Illustrations",
+            JoinDate = new DateOnly(2021, 4, 1),
+            Followers = 10,
+            Following = 10,
+            IsFollowing = true
+        };
+        BindingContext = UserProfile;
+    }
 }
